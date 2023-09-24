284455ac57f7505ea66f85df3506d3a15835411d
Rando Vanja <randovanikov@gmail.com>
2023-09-24 17:43:04 +0300

    add week 3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.WriteLine("Ender a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int result = userNum % 2;
+
+if (result != 0)   //ei ole võrdne
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("user number is even.");
+}
---
+++ Week3IfandSwitch/Program.cs
@@ -0,0 +1,9 @@
+﻿//modules - %
+
+int a = 25;
+int b = 8;
+int c = 9;
+
+int d = a % b;
+
+Console.WriteLine(d);
